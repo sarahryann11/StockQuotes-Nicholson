7ff4d100d9b080618ca826f58d1acefc32c84a11
Sarah Nicholson <sarahryann11@outlook.com>
2018-03-15 14:52:39 -0400

    updating code

--- app/src/main/java/csci490/stockquotes_nicholson/MainActivity.java
+++ app/src/main/java/csci490/stockquotes_nicholson/MainActivity.java
@@ -1,26 +1,16 @@
 package csci490.stockquotes_nicholson;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
-
-    String symbolStr;
-    String nameStr;
-    String priceStr;
-    String timeStr;
-    String changeStr;
-    String rangeStr;
 
     private TextView symbol;
     private TextView name;
@@ -46,51 +36,12 @@ public class MainActivity extends AppCompatActivity {
         editText.setOnKeyListener(new OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                //Stock stock;
                 loadStockQuote quote = new loadStockQuote(MainActivity.this, symbol, name, price, time, change, range);
                 quote.execute(editText.getText().toString());
-                /*try {
-                    stock = quote.get();
-                    if (stock != null && stock.getSymbol() != "" && stock.getSymbol().length() <= 4) {
-                        symbol.setText(symbolStr);
-                        name.setText(nameStr);
-                        price.setText(priceStr);
-                        time.setText(timeStr);
-                        change.setText(changeStr);
-                        range.setText(rangeStr);
-                        Toast toast = Toast.makeText(MainActivity.this, "Quote for " + nameStr, Toast.LENGTH_SHORT);
-                        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-                        toast.show();
-                    } else {
-                        return false;
-                    }
-                } catch (Exception e) {
-                }*/
                 return true;
             }
         });
     }
-
-    /*private class loadStockQuote extends AsyncTask<String, Void, Stock> {
-        @Override
-        protected Stock doInBackground(String... params) {
-
-            Stock stock = new Stock(params[0]);
-            try {
-                stock.load();
-                symbolStr = stock.getSymbol();
-                nameStr = stock.getName();
-                priceStr = stock.getLastTradePrice();
-                timeStr = stock.getLastTradeTime();
-                changeStr = stock.getChange();
-                rangeStr = stock.getRange();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-            return stock;
-        }
-    }*/
 
     @Override
     protected void onSaveInstanceState(Bundle outState){
